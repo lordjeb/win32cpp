2aa4cd526067f67cf13d7c1a067ea08db54560cb
Jeremy Hurren <lordjeb@lordjeb.com>
2017-11-05 14:19:04 -0700

    Add IoC for semaphores

--- src/lib/ioc/semaphore.cpp
+++ src/lib/ioc/semaphore.cpp
@@ -1,8 +1,23 @@
-#include "file_mapping.h"
+#include "semaphore.h"
 
 namespace win32cpp
 {
-    inline namespace ioc
-    {
-    }
+	inline namespace ioc
+	{
+		auto SemaphoreImpl::CreateSemaphoreA(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCSTR lpName) ->HANDLE
+		{
+			return ::CreateSemaphoreA(lpSemaphoreAttributes, lInitialCount, lMaximumCount, lpName);
+		}
+
+		auto SemaphoreImpl::CreateSemaphoreW(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCWSTR lpName) ->HANDLE
+		{
+			return ::CreateSemaphoreW(lpSemaphoreAttributes, lInitialCount, lMaximumCount, lpName);
+		}
+
+		auto SemaphoreImpl::ReleaseSemaphore(_In_ HANDLE hSemaphore, _In_ LONG lReleaseCount, _Out_opt_ LPLONG lpPreviousCount) ->BOOL
+		{
+			return ::ReleaseSemaphore(hSemaphore, lReleaseCount, lpPreviousCount);
+		}
+
+	}
 }
--- src/lib/ioc/semaphore.h
+++ src/lib/ioc/semaphore.h
@@ -8,6 +8,17 @@ namespace win32cpp
         class ISemaphore
         {
         public:
-        }
+			virtual auto CreateSemaphoreA(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCSTR lpName)->HANDLE = 0;
+			virtual auto CreateSemaphoreW(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCWSTR lpName)->HANDLE = 0;
+			virtual auto ReleaseSemaphore(_In_ HANDLE hSemaphore, _In_ LONG lReleaseCount, _Out_opt_ LPLONG lpPreviousCount)->BOOL = 0;
+		};
+
+		class SemaphoreImpl : public ISemaphore
+		{
+		public:
+			virtual auto CreateSemaphoreA(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCSTR lpName)->HANDLE override;
+			virtual auto CreateSemaphoreW(_In_opt_ LPSECURITY_ATTRIBUTES lpSemaphoreAttributes, _In_ LONG lInitialCount, _In_ LONG lMaximumCount, _In_opt_ LPCWSTR lpName)->HANDLE override;
+			virtual auto ReleaseSemaphore(_In_ HANDLE hSemaphore, _In_ LONG lReleaseCount, _Out_opt_ LPLONG lpPreviousCount)->BOOL override;
+		};
     }
 }
