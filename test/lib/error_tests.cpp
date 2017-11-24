17247aebaa103658432f12ea913459365949daa7
Jeremy Hurren <lordjeb@lordjeb.com>
2017-11-23 18:32:33 -0700

    Fix warnings in tests

--- test/lib/error_tests.cpp
+++ test/lib/error_tests.cpp
@@ -12,29 +12,29 @@ TEST(error_test, getErrorMessage_converts_win32_code_to_error_message)
 TEST(error_test, check_bool)
 {
 	EXPECT_THROW(CHECK_BOOL(false), check_failed);
-	EXPECT_NO_THROW(CHECK_BOOL(true), check_failed);
+	EXPECT_NO_THROW(CHECK_BOOL(true));
 }
 
 TEST(error_test, check_count)
 {
 	EXPECT_THROW(CHECK_COUNT(0), check_failed);
-	EXPECT_NO_THROW(CHECK_COUNT(1), check_failed);
+	EXPECT_NO_THROW(CHECK_COUNT(1));
 }
 
 TEST(error_test, check_hr)
 {
 	EXPECT_THROW(CHECK_HR(E_FAIL), check_failed);
-	EXPECT_NO_THROW(CHECK_HR(S_OK), check_failed);
+	EXPECT_NO_THROW(CHECK_HR(S_OK));
 }
 
 TEST(error_test, check_win32)
 {
 	EXPECT_THROW(CHECK_WIN32(ERROR_FILE_NOT_FOUND), check_failed);
-	EXPECT_NO_THROW(CHECK_WIN32(ERROR_SUCCESS), check_failed);
+	EXPECT_NO_THROW(CHECK_WIN32(ERROR_SUCCESS));
 }
 
 TEST(error_test, check_equal)
 {
 	EXPECT_THROW(CHECK(0, 1), check_failed);
-	EXPECT_NO_THROW(CHECK(0, 0), check_failed);
+	EXPECT_NO_THROW(CHECK(0, 0));
 }
