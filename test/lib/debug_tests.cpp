301e6421ddcee8157590b650d0c8d759bfb934ea
Jeremy Hurren <lordjeb@lordjeb.com>
2017-11-26 00:39:29 -0700

    Add debugging tests

---
+++ test/lib/debug_tests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "debug.h"
+
+TEST(debug_test, assertions)
+{
+	ASSERT(true);
+}
+
+TEST(debug_test, tracing)
+{
+	TRACE(L"This line will output to the debugger only in debug builds\n");
+
+	RELTRACE(L"This line will output to the debugger in all builds\n");
+	RELTRACE(L"%s\n", L"Tracing macros can be used with format specifier strings");
+}
+
+TEST(debug_test, debug_output)
+{
+	outputDebugStringEx(L"This is a %s\n", L"test");
+}
