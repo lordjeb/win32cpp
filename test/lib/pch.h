a06350dcac13a59853d4f1a99ba15273ff51b43f
Jeremy Hurren <jehurren@microsoft.com>
2019-09-27 12:28:08 -0600

    Turn off code analysis for googletest headers

--- test/lib/pch.h
+++ test/lib/pch.h
@@ -4,8 +4,12 @@
 #include <memory>
 
 // Googletest
+#include <codeanalysis\warnings.h>
+#pragma warning(push)
+#pragma warning(disable : ALL_CODE_ANALYSIS_WARNINGS)
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#pragma warning(pop)
 using namespace testing;
 
 // Windows
