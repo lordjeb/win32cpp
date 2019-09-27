a17ff03060ef5c92226dce19c1bdcec7a7b490c3
Jeremy Hurren <jehurren@microsoft.com>
2019-09-27 12:28:48 -0600

    Use unsigned printf format specifier for line numbers

--- src/lib/debug.cpp
+++ src/lib/debug.cpp
@@ -8,15 +8,16 @@ win32cpp::tracer::tracer(wchar_t const* filename, unsigned const line) : m_filen
 
 auto win32cpp::tracer::operator()(wchar_t const* pFormat, ...) const -> void
 {
+    constexpr const auto format_string = L"%s(%u): ";
     va_list args;
     va_start(args, pFormat);
-    auto cch1 = _scwprintf(L"%s(%d): ", m_filename, m_line);
+    auto cch1 = _scwprintf(format_string, m_filename, m_line);
     CHECK_NE(-1, cch1);
     auto cch2 = _vscwprintf(pFormat, args);
     CHECK_NE(-1, cch2);
     auto cch = cch1 + cch2 + 1;
     auto pString = std::make_unique<wchar_t[]>(cch);
-    CHECK_NE(-1, swprintf_s(pString.get(), cch, L"%s(%d): ", m_filename, m_line));
+    CHECK_NE(-1, swprintf_s(pString.get(), cch, format_string, m_filename, m_line));
     CHECK_NE(-1, _vsnwprintf_s(pString.get() + cch1, cch - cch1, _TRUNCATE, pFormat, args));
     OutputDebugStringW(pString.get());
     va_end(args);
