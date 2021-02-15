df459c78dd4737353aec7624f2f3505128b20929
lordjeb <lordjeb@lordjeb.com>
2021-02-14 17:01:19 -0700

    Fix compiler error with latest vs2019

--- src/lib/error.cpp
+++ src/lib/error.cpp
@@ -2,6 +2,7 @@
 #include "error.h"
 #include "memory.h"
 #include "string_extensions.h"
+#include <ctype.h>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ wstring win32cpp::getErrorMessage(DWORD errorCode, LANGID languageId /*= LANGID_
     auto flags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM;
     auto x = unique_ptr<wchar_t, heap_ptr_deleter>{};
     CHECK_COUNT(FormatMessageW(flags, nullptr, errorCode, languageId, (LPWSTR)&ptr_setter(x), 0, nullptr));
-    return trimRight(static_cast<const wchar_t*>(x.get()), isspace);
+    return trimRight(static_cast<const wchar_t*>(x.get()), ::isspace);
 }
 
 win32cpp::check_failed::check_failed(long result, const wchar_t* message)
